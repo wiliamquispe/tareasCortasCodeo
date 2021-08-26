e1a685f83e5cc81d9077851ef83925a7e2bb948f
PietroUNMSM <pierocuray52@gmail.com>
2021-08-25 21:50:55 -0500

    +Camino Ponderado

--- CicloV/DAA/tareaSemana12.cpp
+++ CicloV/DAA/tareaSemana12.cpp
@@ -19,12 +19,14 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include <ctype.h>
 
 using namespace std;
 
 const int maxsize = 100;
 const int null = 0;
 
+/*--------------- Registro de Nodo de Huffman ---------------*/
 struct huff_node {
    char symbol;
    int freq;
@@ -32,15 +34,41 @@ struct huff_node {
    char childtype;
 };
 
+
+/*--------------- Registro de nodo de árbol ---------------*/
+struct NODO{
+	int val;
+	struct NODO *izq;
+	struct NODO *der;
+};
+
+
 typedef huff_node *ptr;
 
-/*--------------- PROTOTIPOS ---------------*/
+	/*--------------- PROTOTIPOS ---------------*/
+/*--------------- Metodos delÁrbol ---------------*/
+void crearArbol(NODO **raiz);
+NODO* nuevoNodo(int dato);
+void nuevoArbol(NODO **raiz, NODO* ramaIzq, int x, NODO* ramaDer);
+
+/*                            Metodos de Camino Ponderado
+-------------------------------------------------------------------------------*/
+void comprobarArbolExtendido(NODO *raiz,bool &validado);
+void calcularLCP(NODO *raiz, int &cont, int &caminoPon);
+bool tieneAmbosHijos(NODO *raiz);
+bool esHoja(NODO *raiz);
+void opcionesMenuCPonderado();
+void menuCPonderado();
+
+
+
+	/*--------------- Metodos de Algoritmo de Huffman ---------------*/
 /*--------------- Metodos básicos ---------------*/
 void create(int k, ptr node[]);
 void print(int k, ptr node[]);
 void twosmall(ptr &p, ptr &q, int numnode, ptr node[]);
 
-/*--------------- Metodos de Algoritmo de Huffman ---------------*/
+
 int leerNroSimbolos();
 void crearSimbolos(int numsymbols, ptr node[]);
 void ordenarHuffman(int numsymbols, ptr node[]);
@@ -64,6 +92,160 @@ int main() {
 	system("pause");
 	return(0);
 }
+
+/*--------------- DESARROLLO DE METODOS ---------------*/
+void crearArbol(NODO **raiz)  //Inicializa la raiz con valor NULL
+{
+   *raiz=NULL;
+}
+
+NODO* nuevoNodo(int dato)     // Crea y retorna un nodo con un dato de tipo int y apuntadores izq y der NULL
+{
+   NODO *neo;
+   neo=(NODO*)malloc(sizeof(NODO));
+   neo->val = dato;
+   neo->izq = NULL;
+   neo->der = NULL;
+
+   return neo;
+}
+
+void nuevoArbol(NODO **raiz, NODO* ramaIzq, int x, NODO* ramaDer)
+{
+    *raiz = nuevoNodo(x);
+    (*raiz) -> izq = ramaIzq;
+    (*raiz) -> der = ramaDer;
+}
+
+void comprobarArbolExtendido(NODO *raiz,bool &validado)
+{
+    if(raiz!=NULL){
+        if(tieneAmbosHijos(raiz)){
+            comprobarArbolExtendido(raiz->izq,validado);
+            comprobarArbolExtendido(raiz->der,validado);
+        }
+        else if(!esHoja(raiz)){
+            printf("\n\tEl arbol no es una arbol extendido");
+            validado = false;
+            return;
+        }
+    }
+}
+void calcularLCP(NODO *raiz, int &cont, int &caminoPon){
+    if(raiz!=NULL){
+        if(esHoja(raiz)){
+            caminoPon += cont*raiz->val;
+            cont--;
+        }
+        else{
+            cont++;
+            calcularLCP(raiz->izq,cont,caminoPon);//
+            cont++;
+            calcularLCP(raiz->der,cont,caminoPon);
+            cont--;
+        }    
+    }
+}
+
+bool tieneAmbosHijos(NODO *raiz){
+    if(raiz->izq!=NULL && raiz->der!=NULL)
+        return true;
+    else
+        return false;
+}
+bool esHoja(NODO *raiz)    // Retorna true si el nodo es hoja
+{
+   if(raiz->izq==NULL && raiz->der==NULL ){
+      return true;
+   }
+   else{
+      return false;
+   }
+}
+
+void opcionesMenuCPonderado()
+{
+   printf("\t===================================================\n");
+   printf("\t\t\tLONGITUD DE CAMINO PONDERADO\n");
+   printf("\t===================================================\n");
+   printf("\t[0] Salir\n");
+   printf("\t[1] Crear arbol \n");
+   printf("\t[2] Insertar nodos del arbol\n");
+   printf("\t[3] Comprobar arbol extendido\n");
+   printf("\t[4] Calcular camino ponderado\n");
+   
+   printf("\t---------------------------------------------------\n");
+}
+
+void menuCPonderado()
+{
+   NODO *a;
+   bool validado = true;
+   int cont = 0, caminoPon = 0;
+   char opc;
+   // Nodos para crear el arbol
+   NODO *aizq, *ader;
+
+   do {
+      opcionesMenuCPonderado();
+      printf("\tElija una opcion => ");
+      scanf("%s",&opc);
+      switch(opc) {
+         case '0':
+            printf("\n\tRETORNANDO AL MENU PRINCIPAL...");
+            break;
+
+         case '1':
+            crearArbol(&a);
+            printf("\n\tArbol creado con exito");
+            break;
+
+         case '2':
+            
+            ader = nuevoNodo(7);
+            aizq = nuevoNodo(2);
+            nuevoArbol(&a,aizq,1,ader);
+            
+            aizq->izq = nuevoNodo(6);
+            aizq->der = nuevoNodo(4);
+
+            aizq->izq->izq = nuevoNodo(3);
+            aizq->izq->der = nuevoNodo(5);
+            
+            if(a!=NULL){
+                printf("\n\tNodos insertados\n");
+            }
+            break;
+         case '3':
+            comprobarArbolExtendido(a, validado);
+            if(validado){
+                printf("\n\tEs un arbol extendido\n");
+            }
+            break;
+
+         case '4':
+            if(validado){
+                calcularLCP(a,cont,caminoPon);
+                printf("\n\tLa longitud del camino ponderado es: %d\n",caminoPon);
+            }
+            else
+                printf("\n\tEl arbol no es extendido, no se puede calcular la longitud ponderada\n");
+            break;
+
+         default:
+            printf("\n\tOpcion no valida.\n");
+            break;
+      }
+      printf("\n\n");
+      //system("pause");
+      getch();
+      system("cls");
+   } while(opc!='0');
+}
+
+
+
+
 
 void crearSimbolos(int numsymbols, ptr node[]) {
 	for (int i = 0; i < numsymbols; i++) {
@@ -204,7 +386,11 @@ void menu(){
 				break;	
 			case 3: printf("TBA Quicksort");
 				break;
-			case 4: printf("TBA Camino ponderado");
+			case 4: 
+				system("cls");
+				printf("\t===================================================\n");
+  				 printf("\t\t\tSUBMENU\n");
+  				menuCPonderado();
 				break;
 			case 5:
 				numsymbols = leerNroSimbolos();
